88a3f3946e4c149eefbdc14c7b5b067776310047
Alvinath21 <alvinath098@gmail.com>
2022-05-18 20:15:39 +0700

    Update pemesanan.py

--- pemesanan.py
+++ pemesanan.py
@@ -1,5 +1,14 @@
 from jadwal import Jadwal
+from bis import Bis
+class Pemesanan(Jadwal, Bis):
+   def __init__(self, id_bis: str, id_jadwal: str, harga: str, tglpesanan: str):
+      super().__init__(id_bis, id_jadwal)
+      self.harga = harga
+      self.tglpesanan = tglpesanan
 
-class Pemesanan(Jadwal):
-   def __init__(self):
+   def batalkan(self):
       pass
+
+   def cetak(self):
+      print(f"Harga tiket Bis {self.id_bis} dengan keberangkatan {self.id_jadwal} senilai {self.harga}")
+      print(f"Tiket di pesan pada tanggal {self.tglpesanan}")
