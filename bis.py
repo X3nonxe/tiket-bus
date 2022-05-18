d7cf78b8ee93185d8f3495d6a5cf3ba5b3e201ec
Alvinath21 <alvinath098@gmail.com>
2022-05-18 19:53:43 +0700

    Update bis.py

--- bis.py
+++ bis.py
@@ -1,3 +1,19 @@
 class Bis:
-   def __init__(self):
-      print("Hello World")
+   def __init__(self, id_bis: str, kode_bis: str, plat_bis: str):
+      self.id_bis = id_bis
+      self.kode_bis = kode_bis
+      self.plat_bis = plat_bis
+
+   def pemberangkatan(self):
+      pass
+
+   def tambahdatabis(self):
+      pass
+
+   def editdatabis(self, idbis_baru: str, kodebis_baru: str, platbis_baru: str):
+      self.idbis_baru = idbis_baru
+      self.kodebis_baru = kodebis_baru
+      self.platbis_baru = platbis_baru
+
+   def hapusdatabis(self):
+      pass
