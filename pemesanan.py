b8960c267ac01f87770b4ecd998d4c371b0c9388
Alvinath21 <alvinath098@gmail.com>
2022-05-19 16:37:47 +0700

    Update pemesanan.py
    Bagian pembatalan tiket masih belum

--- pemesanan.py
+++ pemesanan.py
@@ -1,46 +1,25 @@
 from jadwal import Jadwal
 from bis import Bis
+from db import *
 
 class Pemesanan(Jadwal):
-
-
-   # Atribut yang dibutuhkan:
-   def __init__(self, id_bis, id_jadwal, harga, tanggal_pemesanan):
-      super().__init__(id_bis, id_jadwal)
-      self.id_bis = id_bis
-      self.id_jadwal = id_jadwal
+   def __init__(self, id_bis: str, id_jadwal: str, harga: str, tgl_pesanan: str):
+      super().__init__(id_jadwal, id_bis)
       self.harga = harga
-      self.tanggal_pemesanan = tanggal_pemesanan
+      self.tgl_pesanan = tgl_pesanan
 
-   # Method yang dibutuhkan:
-   def batalkan_pemesanan(self):
-      # Input user untuk membatalkan pemesanan
-      inp_id_bis = input("Masukkan ID Bis: ")
-      inp_id_jadwal = input("Masukkan ID Jadwal: ")
-      inp_harga = input("Masukkan Harga: ")
-      inp_tanggal_pemesanan = input("Masukkan Tanggal Pemesanan: ")
+   def cetak(self):
+      # mengambil semua data dari tabel pemesanan
+      cursor.execute("SELECT * FROM pemesanan")
+      data = cursor.fetchall()
 
-      print("Tiket berhasil dibatalkan")
-
-   def cetak_tiket(self):
-      # Input user untuk mencetak tiket
-      inp_id_bis = input("Masukkan ID Bis: ")
-      inp_id_jadwal = input("Masukkan ID Jadwal: ")
-      inp_harga = input("Masukkan Harga: ")
-      inp_tanggal_pemesanan = input("Masukkan Tanggal Pemesanan: ")
-
-      # Tampilan tiket yang di cetak
-      print("=== TIKET ===")
-      print(f"ID Bis: {inp_id_bis}")
-      print(f"ID Jadwal: {inp_id_jadwal}")
-      print(f"Harga: {inp_harga}")
-      print(f"Tanggal Pemesanan: {inp_tanggal_pemesanan}")
-
-      print("Tiket berhasil dicetak")
+      # mencetak/menampilkan data
+      print("==CETAK PEMESANAN==")
+      for row in data:
+         print("ID Bis: ", row[0])
+         print("ID Jadwal: ", row[1])
+         print("Harga Tiket: ", row[2])
+         print("Tanggal Pemesanan: ", row[3])
 
    def batalkan(self):
       pass
-
-   def cetak(self):
-      print(f"Harga tiket Bis {self.id_bis} dengan keberangkatan {self.id_jadwal} senilai {self.harga}")
-      print(f"Tiket di pesan pada tanggal {self.tglpesanan}")
